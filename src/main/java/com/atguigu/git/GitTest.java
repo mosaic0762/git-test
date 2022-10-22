18e113e8dfc45f6612bbb4cbe57748656fb6be12
“mosaic0762” <571522671@qq.com>
2022-10-22 14:12:11 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,15 @@
+package com.atguigu.git;
+
+/*
+ * @Description： //git练习
+ * @Author: 陶春有
+ * @Date: 2022-10-22 14:03
+ **/
+public class GitTest {
+
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
